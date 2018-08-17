903fd25466b2d02f99e1ff0de8aa499246056d60
jerry <jamooyman@gmail.com>
2018-08-18 00:53:05 +1000

    WORK IN PROGRESS

--- redo.hs
+++ redo.hs
@@ -1,41 +1,60 @@
-import           Control.Monad      (filterM, liftM)
+{-# LANGUAGE ScopedTypeVariables #-}
+
+import           Control.Exception  (IOException (..), catch)
+import           Control.Monad      (filterM, liftM, unless)
 import           Data.Map.Lazy      (adjust, fromList, insert, toList)
 import           Data.Maybe         (listToMaybe)
-import           Debug.Trace        (traceShow)
-import           System.Directory   (doesFileExist, removeFile, renameFile)
+import           Data.Typeable      (typeOf)
+import           GHC.IO.Exception   (IOErrorType (..))
+import           System.Directory   (doesFileExist, getDirectoryContents,
+                                     removeFile, renameFile)
 import           System.Environment (getArgs, getEnvironment)
 import           System.Exit        (ExitCode (..))
 import           System.FilePath    (hasExtension, replaceBaseName,
-                                     takeBaseName)
+                                     takeBaseName, (</>))
 import           System.IO          (hPutStrLn, stderr)
+import           System.IO.Error    (ioeGetErrorType)
+
 import           System.Process     (CreateProcess (..), createProcess, shell,
                                      waitForProcess)
 
-traceShow' :: Show b => b -> b
-traceShow' arg = traceShow arg arg
-
 main :: IO ()
 main = mapM_ redo =<< getArgs
+
 redo :: String -> IO ()
-redo target = maybe printMissing redo' =<< redoPath target
+redo target = do
+  upToDate' <- upToDate target
+  unless upToDate' $ maybe printMissing redo' =<< redoPath target
  where
     redo' :: FilePath -> IO ()
     redo' path = do
       oldEnv <- getEnvironment
       let newEnv = toList $ adjust (++ ":.") "PATH" $ insert "REDO_TARGET" target $ fromList oldEnv
-      (_, _, _, ph) <- createProcess $ traceShow' $ (shell $ cmd path) {env = Just newEnv}
+      (_, _, _, ph) <- createProcess $ (shell $ cmd path) {env = Just newEnv}
       exit <- waitForProcess ph
-      case traceShow' exit of
+      case exit of
         ExitSuccess -> renameFile tmp target
         ExitFailure code -> do
           hPutStrLn stderr $ "Redo script exited with non-zero exit code: " ++ show code
           removeFile tmp
     tmp = target ++ "---redoing"
     printMissing = error $ "no .do file found for target '" ++ target ++ "'"
-    cmd path = traceShow' $ unwords ["sh", path, "0", takeBaseName target, tmp, ">", tmp]
+    cmd path = unwords ["sh", path, "0", takeBaseName target, tmp, ">", tmp]
 
 
 redoPath :: FilePath -> IO (Maybe FilePath)
 redoPath target = listToMaybe `liftM` filterM doesFileExist candidates
   where candidates = (target ++ ".do") : [replaceBaseName target "default" ++ ".do" | hasExtension target]
+
+upToDate :: String -> IO Bool
+upToDate target = catch
+  (do deps <- getDirectoryContents depDir
+      all id `liftM` mapM depUpToDate deps)
+  (\(_ :: IOException) -> return False)
+    where depDir = ".redo/" </> target
+          depUpToDate :: FilePath -> IO Bool
+          depUpToDate dep = catch
+            (do oldMD5 <- readFile $ depDir </> dep
+                return False)
+            (\e -> return $ ioeGetErrorType e == InappropriateType)
 
