03f722bab014679c9f597361e6d6bf7111dfc253
jerry <jamooyman@gmail.com>
2018-08-20 14:18:16 +1000

    WORK IN PROGRESS

--- README.markdown
+++ README.markdown
@@ -29,8 +29,24 @@ This project was built by following Jekor's haskell from scratch tutorial.
 
 * episode 9
 
+ref: https://github.com/jekor/redo/tree/ep09
+
 * episode 10
 - recusion. rebuilding dependencies if required
 - building an opensource project (redis) by using redo instead of make.
+
+ref: https://github.com/jekor/redo/tree/ep10
+
+* episode 11
+- replacement of Make
+
+ref:
+
+https://www.youtube.com/watch?annotation_id=annotation_911185&feature=iv&src_vid=nHZ7CtFnI48&v=yH3xQt7XUrc
+
+
+* episode 12
+- cabal and hackage
+
 
 
--- redo.hs
+++ redo.hs
@@ -4,7 +4,7 @@
 import           Control.Exception    (IOException (..), catch, catchJust)
 import           Control.Monad        (filterM, guard, liftM, unless)
 import qualified Data.ByteString.Lazy as BL
-import           Data.Digest.Pure.MD5 (md5)
+import qualified Data.Digest.Pure.MD5 as MD5
 import           Data.Map.Lazy        (adjust, fromList, insert, toList)
 import           Data.Maybe           (listToMaybe)
 --import           Data.Typeable        (typeOf)
@@ -12,16 +12,17 @@ import           Data.Maybe           (listToMaybe)
 import           Debug.Trace          (traceShow)
 import           GHC.IO.Exception     (IOErrorType (..))
 import           System.Directory     (createDirectoryIfMissing, doesFileExist,
+                                       getCurrentDirectory,
                                        getDirectoryContents,
                                        removeDirectoryRecursive, removeFile,
-                                       renameFile)
+                                       renameFile, setCurrentDirectory)
 import           System.Environment   (getArgs, getEnvironment, getProgName,
                                        lookupEnv)
 import           System.Exit          (ExitCode (..))
 import           System.FilePath      (hasExtension, replaceBaseName,
-                                       takeBaseName, (</>))
-import           System.IO            (IOMode (..), hGetLine, hPutStrLn, stderr,
-                                       withFile)
+                                       splitFileName, takeBaseName, (</>))
+import           System.IO            (IOMode (..), hFileSize, hGetLine,
+                                       hPutStrLn, stderr, withFile)
 import           System.IO.Error      (ioeGetErrorType, isDoesNotExistError)
 
 import           System.Process       (CreateProcess (..), createProcess, shell,
@@ -35,34 +36,42 @@ metaDir = ".redo"
 
 main :: IO ()
 main = do
-  mapM_ redo =<< getArgs
+  topDir <- getCurrentDirectory
+  getArgs >>= mapM_ (\arg -> do
+    let (dir, file) = splitFileName arg
+    setCurrentDirectory dir
+    redo file dir
+    setCurrentDirectory topDir)
   progName <- getProgName
   redoTarget' <- lookupEnv "REDO_TARGET"
   case (progName, redoTarget') of
       ("redo-ifchange", Just redoTarget) -> mapM_ (writeMD5 redoTarget) =<< getArgs
       ("redo-ifchange", Nothing) -> error "Missing REDO_TARGET environment variable"
       _ -> return ()
-  where writeMD5 redoTarget dep = writeFile (metaDir </> redoTarget </> dep) =<< md5' dep
 
-redo :: String -> IO ()
-redo target = do
+redo :: String -> FilePath -> IO ()
+redo target dir = do
   upToDate' <- upToDate target
   unless upToDate' $ maybe missingDo redo' =<< doPath target
   where
       redo' :: FilePath -> IO ()
       redo' path = do
+        hPutStrLn stderr $ "redo " ++ show (dir </> target)
         catchJust (guard . isDoesNotExistError)
                   (removeDirectoryRecursive metaDepsDir)
                   (\_ -> return ())
         createDirectoryIfMissing True metaDepsDir
-        writeFile (metaDepsDir </> path) =<< md5' path
-
+        writeMD5 target path
         oldEnv <- getEnvironment
         let newEnv = toList $ adjust (++ ":.") "PATH" $ insert "REDO_TARGET" target $ fromList oldEnv
         (_, _, _, ph) <- createProcess $ (shell $ cmd path) {env = Just newEnv}
         exit <- waitForProcess ph
         case exit of
-          ExitSuccess -> renameFile tmp target
+          ExitSuccess -> do
+            size <- fileSize tmp
+            if size > 0
+            then renameFile tmp target
+            else removeFile tmp
           ExitFailure code -> do
             hPutStrLn stderr $ "Redo script exited with non-zero exit code: " ++ show code
             removeFile tmp
@@ -71,7 +80,7 @@ redo target = do
       missingDo = do
         exists <- doesFileExist target
         unless exists $ error $ "no .do file found for target '" ++ target ++ "'"
-      cmd path = traceShow' $ unwords ["sh -x", path, "0", takeBaseName target, tmp, ">", tmp]
+      cmd path = unwords ["sh", path, "0", takeBaseName target, tmp, ">", tmp]
 
 
 doPath :: FilePath -> IO (Maybe FilePath)
@@ -82,14 +91,14 @@ upToDate :: FilePath -> IO Bool
 upToDate target = catch
   (do exists <- doesFileExist target
       if exists
-      then do deps <- getDirectoryContents $ metaDir </> target
-              (traceShow' . and) `liftM` mapM depUpToDate deps
+      then do md5s <- getDirectoryContents $ metaDir </> target
+              and `liftM` mapM depUpToDate md5s
       else return False)
   (\(_ :: IOException) -> return False)
-  where depUpToDate :: FilePath -> IO Bool
-        depUpToDate dep = catch
-          (do oldMD5 <- withFile (metaDir </> target </> dep) ReadMode hGetLine
-              newMD5 <- md5' dep
+  where depUpToDate :: String -> IO Bool
+        depUpToDate oldMD5 = catch
+          (do dep <- withFile (metaDir </> target </> oldMD5) ReadMode hGetLine
+              newMD5 <- fileMD5 dep
               doScript <- doPath dep
               case doScript of
                 Nothing -> return $ oldMD5 == newMD5
@@ -97,7 +106,16 @@ upToDate target = catch
                              return $ (oldMD5 == newMD5) && upToDate')
           (\e -> return $ ioeGetErrorType e == InappropriateType)
 
-md5' :: FilePath -> IO String
-md5' path = (show . md5) `liftM` BL.readFile path
+fileMD5 :: FilePath -> IO String
+fileMD5 path = (show . MD5.md5) `liftM` BL.readFile path
+
+
+writeMD5 redoTarget dep = do
+  md5 <- fileMD5 dep
+  writeFile (metaDir </> redoTarget </> md5) dep
+
+fileSize :: FilePath -> IO Integer
+fileSize path = withFile path ReadMode hFileSize
+
 
 
